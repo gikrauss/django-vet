c0c2b9522e858f8efb1422abebae48e98a9f645a
German Krauss <german@devartis.com>
2014-09-30 19:24:20 -0300

    New models added to admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from core.models import Client, Patient 
+from core.models import Client, Patient, Breed, Specie, Gender 
 
 admin.site.register(Client)
 admin.site.register(Patient)
+admin.site.register(Breed)
+admin.site.register(Specie)
+admin.site.register(Gender)
 
