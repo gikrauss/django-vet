30972e0fd8085546e96443fb3861d610740f56a5
German Krauss <german@devartis.com>
2014-10-04 22:07:30 -0300

    MedicalRecord added to models

--- core/models.py
+++ core/models.py
@@ -1,24 +1,27 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
+from datetime import datetime
 
 
 class Client(models.Model):
-  firstname = models.CharField(max_length=200)
-  lastname = models.CharField(max_length=200, null=True, blank=True)
-  email = models.EmailField(max_length=200, null=True, blank=True)
+  firstname = models.CharField(max_length=200, verbose_name='nombre')
+  lastname = models.CharField(max_length=200, null=True, blank=True, verbose_name='apellido')
+  email = models.EmailField(max_length=200, null=True, blank=True, verbose_name='email')
 
   def __unicode__(self):
     return "%s %s" % (self.firstname, self.lastname)
 
 
 class Specie(models.Model):
-  name = models.CharField(max_length=200)
+  name = models.CharField(max_length=200, verbose_name='nombre')
 
   def __unicode__(self):
     return self.name
 
 
 class Breed(models.Model):
-  name = models.CharField(max_length=200)
+  name = models.CharField(max_length=200, verbose_name='nombre')
   specie = models.ForeignKey(Specie)
 
   def __unicode__(self):
@@ -26,20 +29,29 @@ class Breed(models.Model):
 
 
 class Gender(models.Model):
-  name = models.CharField(max_length=200)
+  name = models.CharField(max_length=200, verbose_name='nombre')
 
   def __unicode__(self):
     return self.name
 
 
 class Patient(models.Model):
-  name = models.CharField(max_length=200)
-  owner = models.ForeignKey(Client)
-  breed = models.ForeignKey(Breed, null=True)
-  gender = models.ForeignKey(Gender, null=True)
-  birthday = models.DateField(null=True)
+  name = models.CharField(max_length=200, verbose_name='nombre')
+  owner = models.ForeignKey(Client, verbose_name='dueño')
+  breed = models.ForeignKey(Breed, null=True, verbose_name='raza')
+  gender = models.ForeignKey(Gender, null=True, verbose_name='sexo')
+  birthday = models.DateField(null=True, verbose_name='fecha de nacimiento')
+  identifier = models.CharField(null=True, max_length=200, verbose_name='identificador')
+  initial_anamnesis = models.TextField(null=True, verbose_name='anamnesis inicial')
 
   def __unicode__(self):
     return self.name
 
 
+class MedicalRecord(models.Model):
+  date = models.DateTimeField(default=datetime.now, verbose_name='fecha')
+  patient = models.ForeignKey(Patient)
+  anamnesis = models.TextField(null=True, verbose_name='anamnesis')
+  exam = models.TextField(null=True, verbose_name='examen')
+  diagnostic = models.TextField(null=True, verbose_name='diagnóstico')
+  ttd = models.TextField(null=True, verbose_name='ttd')
